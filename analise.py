b06ac3fa14d11a2c23fe3234c64d744800ba5ff9
gilgraca-datascience <gilgraca@gmail.com>
2026-09-23 22:11:50 -0300

    feat/analise: Analise exploratoria de dados query_1

--- analise.py
+++ analise.py
@@ -8,9 +8,10 @@ A biblioteca numpy já vem instalada junto com o pandas. Caso não aconteça, in
 Opção 1: Instalação das bibliotecas: (copie o comando abaixo e cole no terminal do VS Code)
     python -m pip install pandas matplotlib seaborn
 Versões utilizadas:
-    numpy  2.5.3
-    matplotlib 3.11.2
-    seaborn 0.13.2
+    pandas      3.0.6
+    numpy       2.5.3
+    matplotlib  3.11.2
+    seaborn     0.13.2
 
 Opção 2: rodar o arquivo requirements.txt (copie o comando abaixo e cole no terminal do VS Code)
     python -m pip install -r requirements.txt
@@ -25,5 +26,23 @@ import seaborn as sns
 
 df = pd.read_csv('query_1.csv')
 
-print(df.head())
-print(df.tail())
+# Análise exploratória dos dados
+
+print(f"As 5 primeiras linhas do DataFrame:\n{df.head()}\n")
+print(f"As 5 últimas linhas do DataFrame:\n{df.tail()}\n")
+print("Informações sobre o DataFrame:")
+df.info()
+print(f"\nTamanho do DataFrame:\n{df.shape}\n")
+print(f"Colunas do DataFrame:\n{df.columns.to_list()}\n")
+print(f"Tipos de dados do DataFrame:\n{df.dtypes}\n")
+print(f"Estatísticas descritivas para colunas numéricas:\n{df.describe(include=['number'])}\n")
+print(f"Estatísticas descritivas para colunas de string e categoria:\n{df.describe(include=['str', 'category'])}\n")
+
+# analisando inconsistências dos dados
+
+print(f"Valores nulos por coluna:\n{df.isnull().sum()}\n")
+print(f"Valores duplicados:\n{df.duplicated().sum()}\n")
+print(f"Cargos únicos:\n{df['CARGO'].unique()}\n")
+print(f"Quantidade de cargos únicos:\n{df['CARGO'].nunique()}\n")
+print(f"Departamentos únicos:\n{df['DEPARTAMENTO'].unique()}\n")
+print(f"Quantidade de departamentos únicos:\n{df['DEPARTAMENTO'].nunique()}\n")
